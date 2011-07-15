f4790b9cde89459c28c02de9e8fc832426344c24
kjellgm <kjell.gunnar.midtlyng@bekk.no>
2011-07-15 15:45:19 +0200

    Fjernet unødvendige kommentarer

--- wicketintro/src/main/java/no/bekk/charlie/CommentsPanel.java
+++ wicketintro/src/main/java/no/bekk/charlie/CommentsPanel.java
@@ -1,6 +1,5 @@
 package no.bekk.charlie;
 
-import java.io.Serializable;
 import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
@@ -8,7 +7,6 @@ import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.Model;
 
 public class CommentsPanel extends Panel {
 
--- wicketintro/src/main/java/no/bekk/charlie/WicketApplication.java
+++ wicketintro/src/main/java/no/bekk/charlie/WicketApplication.java
@@ -1,29 +1,14 @@
 package no.bekk.charlie;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
- * @see no.bekk.charlie.Start#main(String[])
- */
 public class WicketApplication extends WebApplication
 {    
-    /**
-     * Constructor
-     */
-	
-	public static List<Comment> allComments = new ArrayList<Comment>();
+
 	public WicketApplication()
 	{
 	}
 	
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
 	public Class<Guestbook> getHomePage()
 	{
 		return Guestbook.class;
