b6c622192b9d79a5d8259b5960c2e8eb7e9a4844
kjellgm <kjell.gunnar.midtlyng@bekk.no>
2011-07-15 15:44:17 +0200

    Fjernet unødvendige kommentarer

--- wicketintro/src/main/java/no/bekk/charlie/WicketApplication.java
+++ wicketintro/src/main/java/no/bekk/charlie/WicketApplication.java
@@ -2,23 +2,12 @@ package no.bekk.charlie;
 
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
- * @see no.bekk.charlie.Start#main(String[])
- */
 public class WicketApplication extends WebApplication
 {    
-    /**
-     * Constructor
-     */
 	public WicketApplication()
 	{
 	}
 	
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
 	public Class<HelloWorld> getHomePage()
 	{
 		return HelloWorld.class;
